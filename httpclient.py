2573f7350a067fcd205b6030b8ccffb8944c4282
willfenton <wfenton@ualberta.ca>
2020-02-05 14:33:23 -0700

    Done

--- httpclient.py
+++ httpclient.py
@@ -24,6 +24,9 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+# regular expression library
+import re
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -33,7 +36,12 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def __init__(self):
+        # various regular expressions used to parse URLS and HTTP responses
+        self.hostname_re = re.compile(r"(.*):(.*)")
+        self.response_status_line_re = re.compile(r"(HTTP\/(\S+))\s([0-9]{3})\s([A-Z]+)")
+        self.response_header_re = re.compile(r"(.+):\s(.+)")
+        self.response_body_re = re.compile(r"\r\n\r\n([\s\S]*)")
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -68,28 +76,130 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
+        #----- PARSE URL -----#
+        parsed_url = urllib.parse.urlparse(url, scheme="http")
+
+        # extract components of the URL
+        scheme = parsed_url.scheme
+        netloc = parsed_url.netloc
+        path = parsed_url.path if len(parsed_url.path) > 0 else "/"
+        port = parsed_url.port if parsed_url.port != None else 80
+        
+        # if the netloc is of the format host:port , take only the host
+        match = re.match(self.hostname_re, netloc)
+        host = match.group(1) if match else netloc
+
+        # make sure it's using http
+        if scheme != "http":
+            print(f"Invalid scheme \"{parsed_url.scheme}\". Only HTTP is supported.")
+            return
+
+        #----- BUILD HTTP REQUEST -----#
+        status_line = f"GET {path} HTTP/1.1"
+        host_line = f"Host: {host}"
         body = ""
-        return HTTPResponse(code, body)
+
+        request = f"{status_line}\r\n{host_line}\r\n\r\n{body}"
+
+        #----- SEND HTTP REQUEST -----#
+        self.connect(host, port)                # connect to the host
+        self.sendall(request)                   # send the HTTP request
+        response = self.recvall(self.socket)    # receive the HTTP response
+        self.close()                            # close the connection
+
+        #----- PARSE RESPONSE -----$
+        status_line_matches = re.findall(self.response_status_line_re, response)
+        header_matches = re.findall(self.response_header_re, response)
+        body_matches = re.findall(self.response_body_re, response)
+
+        status_code = int(status_line_matches[0][2])    # response status code
+        response_body = body_matches[0]                 # response body
+
+        #----- LOGGING -----#
+        # print(f"URL: {url}")
+        # print(f"URLPARSE: {parsed_url}")
+        # print(f"HOST: {host}")
+        # print(f"PATH: {path}")
+        # print(f"PORT: {port}")
+        # print(f"REQUEST:\n{repr(request)}")
+        # print(f"RESPONSE:\n{repr(response)}")
+        # print(f"STATUS:\n{status_line_matches}")
+        # print(f"HEADERS:\n{header_matches}")
+        # print(f"BODY:\n{body_matches}")
+        print(body)
+
+        return HTTPResponse(status_code, response_body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        #----- PARSE URL -----#
+        parsed_url = urllib.parse.urlparse(url, scheme="http")
+
+        # extract components of the URL
+        scheme = parsed_url.scheme
+        netloc = parsed_url.netloc
+        path = parsed_url.path if len(parsed_url.path) > 0 else "/"
+        port = parsed_url.port if parsed_url.port != None else 80
+        
+        # if the netloc is of the format host:port , take only the host
+        match = re.match(self.hostname_re, netloc)
+        host = match.group(1) if match else netloc
+
+        # make sure it's using http
+        if scheme != "http":
+            print(f"Invalid scheme \"{parsed_url.scheme}\". Only HTTP is supported.")
+            return
+
+        #----- BUILD HTTP REQUEST -----#
+        body = urllib.parse.urlencode(args) if args else ""
+
+        status_line = f"POST {path} HTTP/1.1"
+        host_line = f"Host: {host}"
+        content_type_line = "Content-Type: application/x-www-form-urlencoded"
+        content_length_line = f"Content-Length: {len(body)}"
+
+        request = f"{status_line}\r\n{host_line}\r\n{content_type_line}\r\n{content_length_line}\r\n\r\n{body}"
+
+        #----- SEND HTTP REQUEST -----#
+        self.connect(host, port)                # connect to the host
+        self.sendall(request)                   # send the HTTP request
+        response = self.recvall(self.socket)    # receive the HTTP response
+        self.close()                            # close the connection
+
+        #----- PARSE RESPONSE -----$
+        status_line_matches = re.findall(self.response_status_line_re, response)
+        header_matches = re.findall(self.response_header_re, response)
+        body_matches = re.findall(self.response_body_re, response)
+
+        status_code = int(status_line_matches[0][2])    # response status code
+        response_body = body_matches[0]                 # response body
+
+        #----- LOGGING -----#
+        # print(f"URL: {url}")
+        # print(f"URLPARSE: {parsed_url}")
+        # print(f"HOST: {host}")
+        # print(f"PATH: {path}")
+        # print(f"PORT: {port}")
+        # print(f"REQUEST:\n{repr(request)}")
+        # print(f"RESPONSE:\n{repr(response)}")
+        # print(f"STATUS:\n{status_line_matches}")
+        # print(f"HEADERS:\n{header_matches}")
+        # print(f"BODY:\n{body_matches}")
+        print(body)
+
+        return HTTPResponse(status_code, response_body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
+            return self.GET(url, args)
     
 if __name__ == "__main__":
     client = HTTPClient()
-    command = "GET"
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
